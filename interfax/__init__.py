8b837eda9d293e272eec07570cfe336a395f6c0b
Ricky "Shake'n'Bake" Bobby <39415009+ricky-shake-n-bake-bobby@users.noreply.github.com>
2021-03-13 15:44:36 -0800

    Update __init__.py

--- interfax/__init__.py
+++ interfax/__init__.py
@@ -1,7 +1,7 @@
 from .response import InboundFax, OutboundFax, ForwardingEmail, Document, Image
 from .files import File
 
-__version__ = '1.0.2'
+__version__ = '1.0.1'
 
 from .client import InterFAX  # NOQA
 
